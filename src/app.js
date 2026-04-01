af50a8f2ecd89dca6d3aab24e3fc7cf97d1a02e1
Abdellah <essabdellah77@gmail.com>
2026-04-01 20:59:44 +0200

    change of message

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ let tasks = [
 ];
 
 app.get('/', (req, res) => {
-  res.json({ message: "DevOps Task Manager API is running..." });
+  res.json({ message: "DevOps Task Manager API is running (lab1)" });
 });
 
 app.get('/tasks', (req, res) => {
