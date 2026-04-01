ce6a083f3b81680779b903fe8591bf6e83fea62c
Abdellah <essabdellah77@gmail.com>
2026-04-02 00:04:25 +0200

    Add missing routes file and package-lock.json

--- tests/app.test.js
+++
@@ -1,6 +0,0 @@
-const request = require('supertest');
-const app = require('../src/app');
-test('Health check', async () => {
- const res = await request(app).get('/');
- expect(res.statusCode).toBe(200);
-}); 
